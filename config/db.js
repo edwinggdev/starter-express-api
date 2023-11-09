a218e908f84ae2a8244fec57f23b2bb808893f4f
edwinggdev <edwin_garcia5@hotmail.com>
2023-11-09 11:55:10 -0500

    ajustes subir fotos s3 cyclic

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const URIDB = "mongodb://localhost:27017/selecta"
-// const URIDB = "mongodb+srv://ciclo4:unab2022@cluster0.rv59jh6.mongodb.net/?retryWrites=true&w=majority"
+// const URIDB = "mongodb://localhost:27017/selecta"
+const URIDB = "mongodb+srv://ciclo4:unab2022@cluster0.rv59jh6.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = ()=>{
     const conn = ()=>{
